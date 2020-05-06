21749bbb05604ba873f4167a728a087540e3aa49
Kiran Mulmi <kiran.mulmi@monotype.com>
2020-05-06 22:35:56 +0545

    bug fix

--- src/Routes/AdminRoutes.js
+++ src/Routes/AdminRoutes.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Route, Redirect} from "react-router-dom";
 import {Header} from "../pages/admin/includes/Header";
 import {Footer} from "../pages/admin/includes/Footer";
 import {Sidebar} from "../pages/admin/includes/Sidebar";
+import {isBackendLogin} from "../helper/authorization";
+import axios from "axios";
 
 
 export const AdminRoutes = ({component: Component, ...rest}) => {
-    return localStorage.getItem('auth_token') ?
+
+    const [isLogin, setIsLogin] = useState(true);
+    useEffect(() => {
+        let authToken = localStorage.getItem('auth_token');
+        axios.get(process.env.REACT_APP_API_HOST_URL+'/auth/validate/jwt',  {
+            params: {
+                token: authToken,
+            }
+        })
+            .then((res) => {
+                setIsLogin(true)
+            })
+            .catch(err => {
+                setIsLogin(false)
+            });
+
+
+    }, []);
+
+    return isLogin ?
     (
         <div>
             <Header/>
--- src/helper/authorization.js
+++ src/helper/authorization.js
@@ -1,4 +1,28 @@
+import axios from "axios";
+
 export const authToken = () => {
   let token = localStorage.getItem('auth_token');
   return {Authorization: "Bearer "+ token};
+};
+
+export const isBackendLogin = async () => {
+  let isUserLogin = false;
+
+  let authToken = localStorage.getItem('auth_token');
+  let response  = await axios.get(process.env.REACT_APP_API_HOST_URL+'/auth/validate/jwt',  {
+                    params: {
+                      token: authToken,
+                    }
+                  })
+                      .then((res) => {
+                        return res.data
+                      })
+                      .catch(err => err);
+
+  if(response.success === true) {
+    isUserLogin = true;
+  }
+
+  return isUserLogin;
+
 };
